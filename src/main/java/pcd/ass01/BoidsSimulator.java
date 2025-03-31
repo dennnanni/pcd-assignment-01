af9c64a6d3dcd5e6a52f0a2f60e8990e25657de0
Denise Nanni <denise.nanni1@gmail.com>
2025-03-31 14:24:08 +0200

    removed useless sleep

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -71,11 +71,6 @@ public class BoidsSimulator {
         for (Thread t : workers) {
             t.interrupt();
         }
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         workers.clear();
     }
 
